fac525e4cff8537c56b52330d6c82020d6c136f5
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2023-02-20 09:57:56 -0500

    Remove the old swift serverless backend
    Issue #2

--- oldBackend/common/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# TEMP fixup the runtime and invoke /bin/defaultBuild explicitly.
-# Once the runtime has the fixes needed, a build.sh will not be needed at all and we
-# can just rely on the default build in the supported way.
-set -e
-cp sim-build/* /bin
-/bin/defaultBuild
