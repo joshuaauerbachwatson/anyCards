5bf143ca1f8c3bddd871e2e64fb306d7475beaf8
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2023-03-18 15:39:35 -0400

    Correct omission of appToken on withdraw request

--- anyCards/ServerExchange.swift
+++ anyCards/ServerExchange.swift
@@ -129,7 +129,7 @@ class ServerBasedCommunicator : NSObject, Communicator {
     // Shutdown this communicator.  First stop the polling, then try to withdraw from the game (silently)
     func shutdown() {
         self.timer?.cancel()
-        guard let arg = try? encoder.encode([ argGameToken: gameToken, argPlayer: playerID ]) else {
+        guard let arg = try? encoder.encode([ argAppToken: AppToken, argGameToken: gameToken, argPlayer: playerID ]) else {
         // ignore error and stop trying to withdraw
             return
         }
